97e2f47bce0380e2c685a7cad463efd51b73a2e4
Zeyu Liao <liaoz1@liaoz1-stg4.rose-hulman.edu>
2023-05-06 23:27:02 -0500

    noen

--- FinalProject/LoadData.py
+++ FinalProject/LoadData.py
@@ -22,47 +22,43 @@ def resize_gestures(input_gestures, final_length=100):
     return output_gestures
 
 
-def load_gestures(dataset, root, version_x, version_y, resize_gesture_to_length):
+def load_gestures(dataset, root, len):
+    # dataset folder path. after dowloading the dataset, please put into corresponding folder (dataset_dhg or dataset_shrec)
     if dataset == 'dhg':
       assert 'dataset_dhg' in root
     if dataset == 'shrec':
       assert 'dataset_shrec' in root
     
-    if version_x == '3D':
-        if dataset == 'dhg':
-            pattern = root + '/gesture_*/finger_*/subject_*/essai_*/skeleton_world.txt'
-        elif dataset == 'shrec':
-            pattern = root + '/gesture_*/finger_*/subject_*/essai_*/skeletons_world.txt'
-    else:
-        if dataset == 'dhg':
-            pattern = root + '/gesture_*/finger_*/subject_*/essai_*/skeleton_image.txt'
-        elif dataset == 'shrec':
-            pattern = root + '/gesture_*/finger_*/subject_*/essai_*/skeletons_image.txt'
+    # check wich dataset is using
+    if dataset == 'dhg':
+        pattern = root + '/gesture_*/finger_*/subject_*/essai_*/skeleton_world.txt'
+    elif dataset == 'shrec':
+        pattern = root + '/gesture_*/finger_*/subject_*/essai_*/skeletons_world.txt'
 
-    gestures_filenames = sorted(glob.glob(pattern))
-    gestures = [numpy.genfromtxt(f) for f in gestures_filenames]
-    if resize_gesture_to_length is not None:
-        gestures = resize_gestures(gestures, final_length=resize_gesture_to_length)
+    gfile = sorted(glob.glob(pattern))
+    gestures = [numpy.genfromtxt(f) for f in gfile]
+    if len is not None:
+        gestures = resize_gestures(gestures, final_length=len)
 
-    labels_14 = [int(filename.split('/')[-5].split('_')[1]) for filename in gestures_filenames]
-    labels_28 = [int(filename.split('/')[-4].split('_')[1]) for filename in gestures_filenames]
-    labels_28 = [labels_14[idx_gesture] if n_fingers_used == 1 else 14 + labels_14[idx_gesture] for idx_gesture, n_fingers_used in enumerate(labels_28)]
+    l1 = [int(filename.split('/')[-5].split('_')[1]) for filename in gfile]
+    l2 = [int(filename.split('/')[-4].split('_')[1]) for filename in gfile]
+    # for idx, fingure in enumerate(l2):
+    #     if fingure == 1:
+    #         l2.append(l1[idx])
+    #     else:
+    #         l2.append(14 + l1[idx] )
+    l2 = [l1[idx] if f == 1 else 14 + l1[idx] for idx, f in enumerate(l2)]
+  
+    return gestures, l1, l2
 
-    if version_y == '14' or version_y == 14:
-        return gestures, labels_14
-    elif version_y == '28' or version_y == 28:
-        return gestures, labels_28
-    elif version_y == 'both':
-        return gestures, labels_14, labels_28
-
-def write_data(data, filepath):
-    with open(filepath, 'wb') as output_file:
-        pickle.dump(data, output_file)
-
-gestures, labels_14, labels_28 = load_gestures(dataset='shrec', root='dataset_shrec', version_x='3D', version_y='both', resize_gesture_to_length=100)
-x_train, x_test, y_train_14, y_test_14, y_train_28, y_test_28 = train_test_split(gestures, labels_14, labels_28, test_size=0.15)
+gestures, l1, l2 = load_gestures('shrec', 'dataset_shrec', 100)
+# split dataset
+x_train, x_test, y_train_14, y_test_14, y_train_28, y_test_28 = train_test_split(gestures, l1, l2, test_size=0.15)
 
 # Save the dataset
 data = {'x_train': x_train, 'x_test': x_test, 'y_train_14': y_train_14, 'y_train_28': y_train_28, 'y_test_14': y_test_14, 'y_test_28': y_test_28}
-write_data(data, filepath='dhg_data.pckl')
+# save pre-processed data to dhg_data.pckl, (the other one SHREC2017.pckl is old version, you could ignore it)
+with open('dhg_data.pckl', 'wb') as output_file:
+    pickle.dump(data, output_file)
+
 
